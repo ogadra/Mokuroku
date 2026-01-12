b81ca2c23ed4ad4875a7d27a2abfae3d2882994f
ogadra <61941819+ogadra@users.noreply.github.com>
2026-01-12 23:54:39 +0900

    config: add drizzle.config.ts

---
+++ drizzle.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "drizzle-kit";
+
+const cfConfig = process.env.LOCAL_DB_PATH
+  ? defineConfig({
+      schema: "./src/repository/schema.ts",
+      out: "./src/repostitory/migrations",
+      dialect: "sqlite",
+      dbCredentials: {
+        url: process.env.LOCAL_DB_PATH,
+      },
+    })
+  : defineConfig({
+      schema: "./src/repository/schema.ts",
+      out: "./src/repository/migrations",
+      dialect: "sqlite",
+      driver: "d1-http",
+      dbCredentials: {
+        accountId: process.env.CLOUDFLARE_ACCOUNT_ID ?? "",
+        databaseId: process.env.CLOUDFLARE_DATABASE_ID ?? "",
+        token: process.env.CLOUDFLARE_API_TOKEN ?? "",
+      },
+    } satisfies Config);
+
+export default cfConfig;
